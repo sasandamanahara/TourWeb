ca59844713f1661b62d4da1792e46805e423f3a3
satha-1 <137098235+satha-1@users.noreply.github.com>
2024-06-27 21:32:29 +0530

    docker

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+
+const mongoose = require('mongoose');
+const express = require('express');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+const mongoURI = process.env.MONGO_URI;
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.log(err));
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
